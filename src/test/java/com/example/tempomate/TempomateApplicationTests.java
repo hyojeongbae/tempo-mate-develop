358e9518dd7055dc185bbc597637cb929c9d78eb
hyojeongbae <bhj09270@hanyang.ac.kr>
2024-09-02 04:44:16 +0900

    first commit

---
+++ src/test/java/com/example/tempomate/TempomateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tempomate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempomateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
